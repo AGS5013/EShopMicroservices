504f73698b3d4012936e8a5c87744c71e38b98fa
AGS0513 <phalgun,boddepalli@adventglobal.com>
2024-07-01 17:50:13 +0530

    CatalogApiTestDone

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//configure the HTTP request pipeline
+app.MapCarter();
+
 
 app.Run();
